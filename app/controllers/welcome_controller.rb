db74cb926e8f4b879a8509eda29f46414bc5756f
James Duncan Davidson <james@duncandavidson.com>
2009-11-21 18:08:11 -0800

    Starting to massage recent tweets onto home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
+    response.headers['Cache-Control'] = 'public, max-age=900'
+    tweets = Tweets.new
+    @mentions = tweets.mentions
   end
   
   def tweets
