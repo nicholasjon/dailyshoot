4beebb72feaa976393fd83640ea4158566070f8d
James Duncan Davidson <james@duncandavidson.com>
2009-11-21 22:04:38 -0800

    Sidebar count changes

--- lib/tweets.rb
+++ lib/tweets.rb
@@ -8,14 +8,14 @@ class Tweets
   end
   
   def mentions
-    @client.mentions(:count => 10)
+    @client.mentions(:count => 8)
   end
   
   def tweets
     tweets = @client.user_timeline.select do |tweet| 
       tweet.text =~ /^\d{4}\/\d{2}\/\d{2}:/
     end
-    tweets[0..9]
+    tweets[0..2]
   end
   
 end
