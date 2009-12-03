fefc566603d0c281cbc108a5af46f510d694d1f2
James Duncan Davidson <james@duncandavidson.com>
2009-12-02 17:37:38 -0700

    Continuing with front page redesign and integration of new photo abilities

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,10 @@ class WelcomeController < ApplicationController
   def index
     response.headers['Cache-Control'] = 'public, max-age=900'
     twitter = TwitterAPI.new
-    @mentions = twitter.mentions(:count => 8)
-    @tweets = twitter.tweets
+    #@mentions = twitter.mentions(:count => 8)
+    #@tweets = twitter.tweets
+    @assignment = Assignment.today
+    @photos = Photo.most_recent(30)
   end
   
   # for apps verification. Will kill soon enough.
