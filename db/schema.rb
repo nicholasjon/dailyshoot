8f3b969ee62c769d52ec46375acc38615dc01efd
Mike Clark <mike@clarkware.com>
2009-12-04 14:54:54 -0800

    Add email to suggestions.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091204160530) do
+ActiveRecord::Schema.define(:version => 20091204224704) do
 
   create_table "assignments", :force => true do |t|
     t.string   "text",       :null => false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20091204160530) do
     t.text     "what",       :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
   create_table "users", :force => true do |t|
