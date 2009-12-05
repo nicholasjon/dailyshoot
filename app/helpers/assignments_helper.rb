78d4077ac98e4feaffe5ecb366bcdc7a777781b8
James Duncan Davidson <james@duncandavidson.com>
2009-12-04 18:23:41 -0800

    Styling navigation up on assignments

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -13,7 +13,10 @@ module AssignmentsHelper
     if !@assignment.last? && @assignment.lower_item.published?
       links << link_to("Next Assignment &rarr;", assignment_path(@assignment.position + 1))
     end
-    links.join(nav_link_separator)
+    
+    # Ha, yes. Messy.
+    
+    "<div class=\"assignment-nav\"><span class=\"previous\">#{links[0]}</span> <span class=\"all\">#{links[1]}</span> <span class=\"next\">#{links[2]}</span></div>"
   end
   
 end
