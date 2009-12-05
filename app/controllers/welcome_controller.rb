dd2f6d8ce308efb90e2b42d40d50745f0314be30
James Duncan Davidson <james@duncandavidson.com>
2009-12-04 23:19:22 -0800

    Cleaning out unused variables

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,6 @@ class WelcomeController < ApplicationController
   def index
     response.headers['Cache-Control'] = 'public, max-age=600'
     twitter = TwitterAPI.new
-    #@mentions = twitter.mentions(:count => 8)
-    #@tweets = twitter.tweets
     @assignment = Assignment.today
     @assignment = Assignment.first if @assignment == nil
     @photos = Photo.most_recent(30)
