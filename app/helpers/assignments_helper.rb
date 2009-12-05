99f3557229dca0ae8760bde90400d6776182e0ed
James Duncan Davidson <james@duncandavidson.com>
2009-12-04 23:32:01 -0800

    Moving assignment navigation from a helper method to a full blown partial. There was so much HTML in the new one that it simply made sense.

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,22 +1,3 @@
 module AssignmentsHelper
-  
-  def nav_link_separator
-    %{<span class="separator"> | </span>}
-  end
-  
-  def navigation_links(assignment)
-    links = []
-    unless @assignment.first?
-      links << link_to("&larr; Previous Assignment", assignment_path(@assignment.position - 1))
-    end
-    links << link_to('All Assignments', assignments_path)
-    if !@assignment.last? && @assignment.lower_item.published?
-      links << link_to("Next Assignment &rarr;", assignment_path(@assignment.position + 1))
-    end
     
-    # Ha, yes. Messy.
-    
-    "<div class=\"assignment-nav\"><span class=\"previous\">#{links[0]}</span> <span class=\"all\">#{links[1]}</span> <span class=\"next\">#{links[2]}</span></div>"
-  end
-  
 end
