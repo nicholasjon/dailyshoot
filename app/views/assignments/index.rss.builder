17ac803982a933181b8713e3969e94a88d6d0bb7
Mike Clark <mike@clarkware.com>
2009-12-09 15:58:51 -0700

    Add hints to forms, and also change the RSS entry title.

--- app/views/assignments/index.rss.builder
+++ app/views/assignments/index.rss.builder
@@ -8,7 +8,7 @@ xml.rss :version => "2.0" do
     xml.lastBuildDate @assignments.first.rfc822_date    
     @assignments.each do |assignment|
       xml.item do
-        xml.title "#{assignment.tweet_date}: ##{assignment.tag}"
+        xml.title "#{assignment.tweet_date}: Daily Shoot ##{assignment.tag}"
         xml.description assignment.text
         xml.pubDate assignment.rfc822_date
         xml.link assignment_url(assignment)
--- lib/labeled_form_builder.rb
+++ lib/labeled_form_builder.rb
@@ -4,8 +4,14 @@
 class LabeledFormBuilder < ActionView::Helpers::FormBuilder
   %w[text_field collection_select password_field text_area date_select].each do |method_name|
     define_method(method_name) do |field_name, *args|
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      if options[:hint] 
+        hint = @template.content_tag(:span, options[:hint], :class => "hint")
+      else
+        hint = ''
+      end
       @template.content_tag(:p, field_label(field_name, *args) + 
-                                super + field_error(field_name))
+                                super + hint + field_error(field_name))
     end
   end
   
