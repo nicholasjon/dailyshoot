c253ca4aae33067114bb66664679a0c4741cf761
Mike Clark <mike@clarkware.com>
2009-12-15 11:30:51 -0700

    Change to db-backed sessions.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091209223704) do
+ActiveRecord::Schema.define(:version => 20091215182945) do
 
   create_table "assignments", :force => true do |t|
     t.string   "text",                        :null => false
@@ -57,6 +57,16 @@ ActiveRecord::Schema.define(:version => 20091209223704) do
 
   add_index "photos", ["assignment_id"], :name => "index_photos_on_assignment_id"
   add_index "photos", ["photog_id"], :name => "index_photos_on_photog_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "suggestions", :force => true do |t|
     t.string   "name"
