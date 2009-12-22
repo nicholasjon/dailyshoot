0153f52b4f5c5fe16d97d24daaccd173054b72f2
James Duncan Davidson <james@duncandavidson.com>
2009-12-21 23:17:00 -0800

    Final touches before pushing up and giving it a shot

--- lib/assignment_sender.rb
+++ lib/assignment_sender.rb
@@ -4,15 +4,22 @@ class AssignmentSender
   
   def perform
     assignment = Assignment.today
+    
+    if ENV['RAILS_ENV'] == "development"
+
+    end
 
     if assignment.tweeted_at      
       Rails.logger.warn "Today's assignment w/id #{assignment.id} already tweeted, skipping"
     else
 
       # push assignment to Twitter
-      twitter = TwitterAPI.new
-      #twitter.tweet assignment.as_tweet
-      puts "PRETEND"
+      if ENV['RAILS_ENV'] == 'production'
+        twitter = TwitterAPI.new
+        twitter.tweet assignment.as_tweet
+      else
+        puts "We'll pretend today's assignment tweet went out right now"
+      end
 
       # do our record keeping
       assignment.tweeted_at = Time.now
@@ -26,6 +33,7 @@ class AssignmentSender
     tom = Date.tomorrow
     time = DateTime.civil(tom.year, tom.month, tom.day, 16)
     
+    Rails.logger.info "Scheduling AssignmentSender for #{time}"
     Delayed::Job.enqueue(self, 0, time)
   end
 end
