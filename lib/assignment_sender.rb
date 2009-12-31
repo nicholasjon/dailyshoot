7558382698d67c323fdc4e9d5f041686ca768226
James Davidson <james@duncandavidson.com>
2009-12-31 11:07:10 -0800

    Updating to simplier approach

--- lib/assignment_sender.rb
+++ lib/assignment_sender.rb
@@ -4,23 +4,15 @@ class AssignmentSender
   
   def perform
     assignment = Assignment.today
-    
-    if ENV['RAILS_ENV'] == "development"
-
-    end
 
     if assignment.tweeted_at      
       Rails.logger.warn "Today's assignment w/id #{assignment.id} already tweeted, skipping"
     else
 
-      # push assignment to Twitter
-      if ENV['RAILS_ENV'] == 'production'
-        twitter = TwitterAPI.new
-        twitter.tweet assignment.as_tweet
-      else
-        puts "We'll pretend today's assignment tweet went out right now"
-      end
-
+      twitter = TwitterAPI.new(ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET'],
+      						   ENV['TWITTER_POST_TOKEN'], ENV['TWITTER_POST_SECRET'])
+      twitter.tweet assignment.as_tweet
+      
       # do our record keeping
       assignment.tweeted_at = Time.now
       assignment.save!
@@ -29,11 +21,12 @@ class AssignmentSender
       Rails.logger.info "Sent today's assignment to Twitter"
     end
     
-    # enqueue ourselves for tomorrow
+    # enqueue ourselves for tomorrow. and use GMT time
     tom = Date.tomorrow
-    time = DateTime.civil(tom.year, tom.month, tom.day, 16)
+    time = DateTime.civil(tom.year, tom.month, tom.day, 22)
     
     Rails.logger.info "Scheduling AssignmentSender for #{time}"
     Delayed::Job.enqueue(self, 0, time)
   end
 end
+
