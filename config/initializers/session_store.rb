0547436bcdd5ef391df3f853c1b856786509d9e6
James Davidson <james@duncandavidson.com>
2009-12-31 19:43:52 -0800

    Setting expiration time for sessions to 1 day. Good enough?

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,10 +4,12 @@
 # If you change this key, all old sessions will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
+
 ActionController::Base.session = {
   :key         => '_dailyshoot.com_session',
   :secret      => '69ed3bb75c596a568a3aa33ed6359d1310c4abae64150ffcd153932a1c9d59f5c005c25d3019d309ae1c43afb1f50d30672d0b863c1cf9462b8d0df06c1f27a3',
-  :cache       => CACHE
+  :cache       => CACHE,
+  :expires_after => 1.day
 }
 
 # Use the database for sessions instead of the cookie-based default,
