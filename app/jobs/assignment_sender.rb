bc1efcfa0df63455237207b6491302b8362a1a9d
James Davidson <james@duncandavidson.com>
2010-01-02 15:35:06 -0800

    Adding reminder logic to Assignment Sender. Straightfoward enough

--- app/jobs/assignment_sender.rb
+++ app/jobs/assignment_sender.rb
@@ -2,11 +2,25 @@ require 'twitter_api'
 
 class AssignmentSender
   
+  def initialize(mode=:send)
+    @mode = mode
+  end
+  
   def perform
+    case @mode
+    when :send
+      send_todays_assignment
+    when :reminder
+      remind_todays_assignment
+    else
+      Rails.logger.warn "WhiskeyTangoFoxtrot: unknown AssignmentSender mode #{@mode}"
+    end
+  end
+  
+  def send_todays_assignment
     assignment = Assignment.today
-
     if assignment.tweeted_at      
-      Rails.logger.warn "Today's assignment w/id #{assignment.id} already tweeted, skipping"
+      Rails.logger.warn "Today's assignment w/id #{assignment.id} already tweeted"
     else
      
       TwitterAPI.tweet_as_dailyshoot(assignment.as_tweet)
@@ -21,10 +35,18 @@ class AssignmentSender
     
     # enqueue ourselves for tomorrow. and use GMT time
     tom = Date.tomorrow
-    time = DateTime.civil(tom.year, tom.month, tom.day, 14)
-    
+    time = DateTime.civil(tom.year, tom.month, tom.day, 14) 
     Rails.logger.info "Scheduling AssignmentSender for #{time}"
-    Delayed::Job.enqueue(self, 0, time)
+    Delayed::Job.enqueue(self, 0, time)  
+    
+    # enqueue reminder for 10 hours from now
+    Delayed::Job.enqueue(AssignmentSender.new(:reminder), 0, 10.seconds.from_now)
+  end
+  
+  def remind_todays_assignment
+    assignment = Assignment.today
+    msg = "\##{assignment.tag} #{assignment.text} "
+    TwitterAPI.tweet_as_dailyshoot(msg)
   end
 end
 
