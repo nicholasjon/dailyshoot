6fb5f9ecc921621b1d170c769c2f473e3d403951
Mike Clark <mike@clarkware.com>
2010-01-07 15:49:08 -0700

    Restyle the main mobile page.

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -1,5 +1,9 @@
 module MobileHelper
   
+  def assignment_summary(assignment)
+    "#{assignment.position}. #{truncate(assignment.text, :length => 25)}"
+  end
+  
   def assignment_date(assignment)
     "#{Date::DAYNAMES[assignment.date.wday]}: #{assignment.tweet_date}"
   end
