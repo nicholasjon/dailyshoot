d0665383c166fb7f3706d9952e81eb20973c3374
Mike Clark <mike@clarkware.com>
2010-01-31 13:42:54 -0700

    Add a link to the original tweet when viewing unparsed mentions.

--- app/helpers/mentions_helper.rb
+++ app/helpers/mentions_helper.rb
@@ -1,2 +1,8 @@
 module MentionsHelper
+  
+  def link_to_tweet(mention)
+    link_to mention.screen_name, 
+            "http://twitter.com/#{mention.screen_name}/status/#{mention.tweet_id}"
+  end
+  
 end
